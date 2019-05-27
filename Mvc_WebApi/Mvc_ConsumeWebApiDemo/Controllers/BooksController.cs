4f26d0d656dc8c4aa21a44637fbc86b918c34161
Samson Monballiu <samsonmonballiu@hotmail.com>
2019-05-27 09:46:07 +0200

    installed WebApi.Client NuGet package

--- Mvc_WebApi/Mvc_ConsumeWebApiDemo/Controllers/BooksController.cs
+++ Mvc_WebApi/Mvc_ConsumeWebApiDemo/Controllers/BooksController.cs
@@ -1,7 +1,11 @@
 ﻿using Mvc_ConsumeWebApiDemo.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,6 +22,27 @@ namespace Mvc_ConsumeWebApiDemo.Controllers
         public ActionResult GetBooks()
         {
             IEnumerable<Book> books = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:50070/api/books"); //show the client the way to the WebApi service.
+                var response = client.GetAsync("books");
+                response.Wait(); // wait for the client.GetAsync("books") to be completed.
+
+                var result = response.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var read = result.Content.ReadAsStringAsync();
+                    books = JsonConvert.DeserializeObject<IEnumerable<Book>>(read.Result);
+                }
+
+                else
+                {
+                    books = Enumerable.Empty<Book>();
+                    ModelState.AddModelError(String.Empty, "Server error, please try again later.");
+                }
+            }
+            return View(books);
         }
     }
 }
