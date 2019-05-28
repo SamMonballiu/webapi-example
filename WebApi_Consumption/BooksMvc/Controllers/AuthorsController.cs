916c968d12855ef0496d8498f5b03881f6dc4836
Samson Monballiu <samsonmonballiu@hotmail.com>
2019-05-28 13:55:04 +0200

    Mvc: Books & Authors controllers

---
+++ WebApi_Consumption/BooksMvc/Controllers/AuthorsController.cs
@@ -0,0 +1,33 @@
+﻿using BooksMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooksMvc.Controllers
+{
+    public class AuthorsController : BaseController
+    {
+        // GET: Authors
+        [Route("/authors/{id}")]
+        public ActionResult Index(int? id)
+        {
+            var authors = bookService.GetAuthors().Result;
+            var books = bookService.GetBooks().Result;
+
+            var vm = new AuthorsViewModel()
+            {
+                Authors = authors,
+                SelectedBook = id == null ? null : books.Find(x => x.Id == id)
+            };
+
+            if (vm.SelectedBook != null)
+            {
+                vm.SelectedBook.Author = authors.Find(x => x.Id == vm.SelectedBook.AuthorId); 
+            }
+
+            return View(vm);
+        }
+    }
+}
---
+++ WebApi_Consumption/BooksMvc/Models/AuthorsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DataLayer.Models;
+using System.Collections.Generic;
+
+namespace BooksMvc.Models
+{
+    public class AuthorsViewModel
+    {
+        public List<Author> Authors { get; set; }
+        public Book SelectedBook { get; set; }
+    }
+}
